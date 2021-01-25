cccc78bccd5554bd74d21a91d83b83c33af5a347
LAPTOP-S1IDSC7T\Goodtechnology <dearing919@gmail.com>
2021-01-25 18:03:30 +0800

    D4

---
+++ practice04.py
@@ -0,0 +1,19 @@
+# 載入 numpy 套件
+import numpy as np
+
+# 題目:
+english_score = np.array([55,89,76,65,48,70])
+math_score = np.array([60,85,60,68,55,60])
+chinese_score = np.array([65,90,82,72,66,77])
+# 上3列共六位同學的英文、數學、國文成績，第一個元素代表第一位同學，舉例第一位同學英文55分、數學60分、國文65分，運用上列數據回答下列問題。
+
+#1.有多少學生英文成績比數學成績高?
+
+a = np.greater(english_score , math_score)
+a = np.where(a==True)
+a = np.size(a)
+print(a)
+
+#2.是否全班同學最高分都是國文?
+b = np.all(np.greater(chinese_score,math_score,english_score))
+print(b)
