7d0d1872264bc9205857f7734f6e64e829dff201
LAPTOP-S1IDSC7T\Goodtechnology <dearing919@gmail.com>
2021-02-01 15:47:22 +0800

    no message

--- practice13.py
+++ practice13.py
@@ -14,9 +14,10 @@ score_df = pd.DataFrame([
                         [10,88,40,43]],
 columns=['student_id','math_score','english_score','chinese_score'])
 score_df = score_df.set_index('student_id')
-# a. 6 號學生(student_id=6) 3 科平均分數為何？
+# a. 6 號學生(student_id=6) 3 科平均分數為何？ 
 print(score_df.mean(axis=1)[6])
 # b. 6 號學生 3 科平均分數是否有贏過班上一半的同學？
+# ps可用是否贏過平均分中位數
 print(score_df.mean(axis=1)[6]>score_df.mean(axis=1),True>5)
 # c. 由於班上同學成績不好，所以學校統一加分，加分方式為開根號乘以十請問6號同學3科成績分別是？
 print(score_df.apply(lambda x : x**(0.5)*10)[5:6])
