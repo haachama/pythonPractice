64d6b0ca21a5aa16f2dba5326c81780ac33d7ea8
LAPTOP-S1IDSC7T\Goodtechnology <dearing919@gmail.com>
2021-02-18 15:29:09 +0800

    D21

---
+++ practice21.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# 取得資料集
+df = sns.load_dataset('titanic')
+
+# 題目：取得另一個 dataset：titanic
+df.info()
+
+# 做條形圖
+# 直接使用PANDAS dataframe, 當作參數
+# 條形圖()顯示分類變數和連續變數之間的關係。數據以矩形條表示,其中條的長度表示該類別中數據的比例。
+sns.barplot(data = df,x = "sex", y = "survived",hue = 'class', orient = "v")
+
+# 利用 facet grid 繪圖並分析
+# 瞭解性別在各艙等的分布的存活率
+g = sns.FacetGrid(df, col = "survived")
+g.map(plt.hist,"pclass")
+plt.show()
+
+h = sns.FacetGrid(df, col = "survived")
+h.map(plt.hist,"sex")
+plt.show()
+
+#先檢視各艙位存活人數，此時可以使用groupby函數進行分類，
+#其中 survived＝1表示存活，survived＝0表示死亡，將survived加總即為各艙等生存人數。
+
+df.groupby('pclass').survived.sum()
+
+#加上性別
+survived=df.groupby(['pclass','sex']).survived.sum()
+survived.plot(kind='bar')
+
+#使用pd.crosstab函數繪製交叉表，交叉表可以很直觀的依據艙位等級及性別來查看存活人數及死亡人數。
+#繪製堆疊條形圖，x軸代表依據艙等分成男性及女性，y軸代表人數，其中藍色代表死亡人數，橘色代表存活人數。
+survived_counts = pd.crosstab([df.pclass, df.sex],df.survived)
+print(survived_counts) 
+survived_counts.plot(kind='bar', stacked=True)
+
+
+
+#鬼之繪圖
+n = sns.FacetGrid(data = df, col="survived", hue="survived")
+n.map(sns.histplot, "sex","class",data=df,legend=True,stat="count",cbar=True)
+plt.show()
+
+p = sns.FacetGrid(data = df, col="survived", hue="survived")
+p.map(sns.histplot, "sex","class",data=df,legend=True,stat="probability",cbar=True)
+plt.show()
+
+
+# 繪製小提琴圖
+# 直接使用PANDAS dataframe, 當作參數
+#條形圖()顯示分類變數和連續變數之間的關係。數據以矩形條表示,其中條的長度表示該類別中數據的比例。
+
+sns.violinplot(data=survived_counts)
+plt.show()
+
